66d51b8cb39f396a6c9e1a1ce2c664a5cbee64fd
daisy-dev <kadanarpa@gmail.com>
2024-11-15 18:01:25 -0500

    feat: add handler exceptions for some dtos

--- src/main/java/co/edu/usco/TM/config/LocaleConfig.java
+++ src/main/java/co/edu/usco/TM/config/LocaleConfig.java
@@ -1,4 +1,58 @@
 package co.edu.usco.TM.config;
 
-public class LocaleConfig {
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
+
+@Configuration
+public class LocaleConfig implements WebMvcConfigurer {
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        messageSource.setDefaultEncoding(StandardCharsets.ISO_8859_1.name());
+        return messageSource;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver resolver = new SessionLocaleResolver();
+        resolver.setDefaultLocale(new Locale("es"));
+        return resolver;
+    }
+
+    /**
+     * <h2>Interceptor de cambio de idioma</h2>
+     * Se encarga de leer automáticamente el idioma desde la cabecera <code>Accept-Language</code> si es un cliente
+     * quien la envía, o desde un parámetro en la URL como <code>?lang=es</code>
+     *
+     * <b>Importante:</b> No es necesario agregar la anotación {@link org.springframework.web.bind.annotation.RequestHeader}
+     * entre los parámetros de las funciones anotadas por mapeos de controladores, puesto que gracias a esta configuración
+     * se interceptará el idioma global y automáticamente.
+     *
+     * @return Instancia de LocaleChangeInterceptor que contiene el idioma cambiado al interceptarlo en las cabeceras
+     * de las peticiones HTTP.
+     */
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lang");
+        return interceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
 }
--- src/main/java/co/edu/usco/TM/exceptions/GlobalExceptionHandler.java
+++ src/main/java/co/edu/usco/TM/exceptions/GlobalExceptionHandler.java
@@ -1,30 +1,69 @@
 package co.edu.usco.TM.exceptions;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
+import java.util.Locale;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @Autowired
+    MessageSource messageSource;
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<MessageResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+    public ResponseEntity<MessageResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex, WebRequest webRequest) {
+
+        Locale locale = webRequest.getLocale();
+
+        Map<String, String> errors = getValidationErrors(ex.getBindingResult(), locale);
+
+
         return ResponseEntity.badRequest().body(
                 new MessageResponse(
                         HttpStatus.BAD_REQUEST.value(),
                         "Argumentos no válidos para " + ex.getTarget().getClass().getSimpleName(),
-                        "Errores: " + ex.getBindingResult().getFieldErrors().stream()
-                                .collect(Collectors.toMap(
-                                        FieldError::getField,
-                                        FieldError::getDefaultMessage
-                                )),
+                        "Errores en los campos: " + errors,
                         LocalDateTime.now()));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<MessageResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex, WebRequest webRequest) {
+
+        Locale locale = webRequest.getLocale();
+
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(
+                new MessageResponse(
+                        HttpStatus.UNPROCESSABLE_ENTITY.value(),
+                        getMessage("NotReadable", locale),
+                        ex.getCause().getLocalizedMessage(),
+                        LocalDateTime.now()
+                )
+        );
+    }
+
+    public Map<String, String> getValidationErrors(BindingResult errors, Locale locale) {
+        return errors.getFieldErrors().stream()
+                .collect(Collectors.toMap(
+                        FieldError::getField,
+                        fieldError -> getMessage(fieldError.getCode(), locale))
+                );
+    }
+
+    public String getMessage(String key, Locale locale) {
+        return messageSource.getMessage(key, null, locale);
     }
 
 }
